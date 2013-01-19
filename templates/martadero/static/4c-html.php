e4eac329d75e15bdd887082ca94c4d31710ff424
Carlos E. Caballero B <cijkb.j@gmail.com>
2013-01-19 08:26:47 -0400

    [STATIC] working on redes, section

---
+++ templates/martadero/static/4c-html.php
@@ -0,0 +1,50 @@
+<h1>REDES CULTURALES<br />
+4C - Circuito de Centros Culturales de Cochabamba</h1>
+
+
+<p><img src="<?php echo $this->staticUrl('/marca-4c.jpg') ?>" class="box_image" />
+<strong>Trabajar en red se presenta como una necesidad </strong>en el momento en
+que vivimos. Se ha de dejar atrás la idea de "centro" estrella y ser conscientes
+que es <strong>desde el trabajo interconectado y grupal como mejor conseguiremos
+llevar a cabo nuestra misión</strong>.</p>
+
+<p>El <strong>Circuito de Centros Culturales de Cochabamba</strong> nace en 2009
+con esta finalidad, el de ofrecer una propuesta artística y cultural en conjunto
+a la totalidad de la población cochabambina.</p>
+
+<p><strong>Los centros</strong> que forman parte de esta iniciativa son:</p>
+
+<p><strong>Proyecto mARTadero</strong>:
+<a href="http://www.martadero.org">www.martadero.org</a></p>
+<p><strong>Fundación Imagen</strong>:
+<a href="http://www.fundacionimagen.org" target="_blank">www.fundacionimagen.org </a></p>
+<p><strong>Centro Boliviano Americano</strong>:
+<a href="http://www.cbacoch.org/" target="_blank">www.cbacoch.org/</a></p>
+<p><strong>Alianza Francesa</strong>:
+<a href="http://www.afbolivia.org/alianza-francesa-cochabamba.html" target="_blank">www.afbolivia.org</a></p>
+<p><strong>Instituto Cultural Boliviano Alemán (ICBA)</strong>:
+<a href="http://www.icbacbba.org/" target="_blank">www.icbacbba.org/</a></p>
+<p><strong>Centro Pedagógico y Cultural Simón y Patiño</strong>:
+<a href="http://centropatino.fundacionpatino.org/index.php" target="_blank">centropatino.fundacionpatino.org</a></p>
+
+<p><strong>Además colaboran</strong>: La Fábrica, Museo de Historia Natural,
+Casona Santivañez, Teatro Achá, Teatro Adela Zamudio, Jardín Botánico, Museo de
+Santa Teresa, Auditorio de la T.D.E., Instituto Laredo y el Salón Walter
+Terrazas.</p>
+
+<p><em>Pincha sobre la imagen para descargártela en alta resolución</em></p>
+
+<p><a href="<?php echo $this->staticUrl('/panel-4c.jpg') ?>" class="highslide" onclick="return hs.expand(this);">
+<img src="<?php echo $this->staticUrl('/panel-4c-small.jpg') ?>" alt="4C Circuito de Centros Culturales de Cochabamba" title="4C Circuito de Centros Culturales de Cochabamba"/></a></p>
+
+<p><img src="<?php echo $this->staticUrl('/manzana-verde.jpg') ?>" title="Campaña Manzanas Verdes" class="box_image" />
+Una de las primeras acciones conjuntas llevadas a cabo por 4C ha sido la
+<strong>Campaña de Manzanas Verdes</strong>, un apropuesta de cuidado
+medioambiental de nuestro entorno más inmediato, nuestros barrios y lugares de
+trabajo, involucrando a la sociedad para conseguir entre todos una ciudad más
+limpia y agradable.</p>
+
+<p>En colaboración con:<br />
+<a href="http://www.gaiapacha.org/" target="_blank">
+<img src="<?php echo $this->staticUrl('/logo-gaiapacha.jpg') ?>" alt="Fundación Gaia Pacha" title="Fundación Gaia Pacha" />
+</a></p>
